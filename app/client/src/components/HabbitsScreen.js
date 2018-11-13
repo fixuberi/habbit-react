775de70e2293e30a3b901bb575984cbb3a94fd71
fixuberi <comeclarity007@gmail.com>
2018-11-13 17:18:59 +0200

    fetching habbits list from server

--- app/client/src/components/HabbitsScreen.js
+++ app/client/src/components/HabbitsScreen.js
@@ -2,55 +2,30 @@ import React, {Component} from 'react';
 import HabbitsScreenHeader from './HabbitsScreenHeader';
 import HabbitsTable from './HabbitsTable';
 import Grid from 'react-bootstrap/lib/Grid'
+import axios from'axios';
 
 class HabbitsScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            habbits: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3001/habbits')
+            .then(response => this.setState( {habbits: response.data} ))
+            .catch(error => console.log(error.message));
+    }
+
     render() {
         return(
             <Grid>
                 <HabbitsScreenHeader />
-                <HabbitsTable habbits={table}/>
+                <HabbitsTable habbits={this.state.habbits}/>
             </Grid>
         );
     }
 }
-const table = [
-    {
-        "executions": [
-            {
-                "date": "2018-11-12", 
-                "habbit_id": 1, 
-                "id": 1
-            },
-            {
-                "date": "2018-11-11", 
-                "habbit_id": 1, 
-                "id": 2
-            }
-        ], 
-        "id": 1, 
-        "name": "wake up earlyssssss sssssssss sssssss", 
-    }, 
-    {
-        "executions": [
-            {
-                "date": "2018-11-09", 
-                "habbit_id": 1, 
-                "id": 3
-            }
-        ], 
-        "id": 3, 
-        "name": "do something", 
-    }, 
-    {
-        "executions": [
-            {
-                "date": "2018-11-12", 
-                "habbit_id": 1, 
-                "id": 1
-            }  
-        ], 
-        "id": 4, 
-        "name": "be positivity", 
-    }
-];
+
 export default HabbitsScreen;
